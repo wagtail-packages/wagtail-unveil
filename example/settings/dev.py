215414da34c55940501982e7a1a2127fd4d73d36
Nick Moreton <nickmoreton@me.com>
2025-04-22 22:02:16 +0100

    Initial example project structure

---
+++ example/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *  # noqa: F403
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-f9e^f3ho-3h2m=2w=g@@e3yzshut&fp7i5ur&&)7f21ach=%c)"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *  # noqa: F403
+except ImportError:
+    pass
