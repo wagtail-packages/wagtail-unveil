9c5cb2a02bdfe6bbd8840b6215f778a180bbc3d9
Nick Moreton <nickmoreton@me.com>
2025-04-25 21:32:38 +0100

    Alter modelviewset_models to return only the models, not the URL paths

--- src/wagtail_unveil/management/commands/list_admin_urls.py
+++ src/wagtail_unveil/management/commands/list_admin_urls.py
@@ -140,6 +140,8 @@ class Command(BaseCommand):
             self.stdout.write(f"  - {model.__name__}")
 
         # Get URLs for modelviewset models
+        # Create an empty dictionary for URL paths since we no longer get it from get_modelviewset_models()
+        modelviewset_url_paths = {}
         modelviewset_urls = get_modelviewset_urls(
             self.stdout,
             base_url,
